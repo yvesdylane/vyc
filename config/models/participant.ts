a359e5378e77ecc19b47ea0c756627a503f93385
yvesdylane <yvesdylane24@gmail.com>
2025-07-10 15:59:31 +0100

    models

--- config/models/participant.ts
+++ config/models/participant.ts
@@ -2,7 +2,7 @@ export interface participant {
     id : number; // Unique identifier for the participant
     event_id : number; // ID of the event the participant is associated with
     user_id? : string; // UUID of the user participating in the event, optional cause i don't want to return it in the response
-    name: string; // Name of the participant
+    name?: string; // Name of the participant
     images? : string; // Optional field for participant's images
     bio? : string; // Optional biography of the participant
     vote_power? : number; // Optional field for the participant's voting power
