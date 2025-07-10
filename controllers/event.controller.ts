adc46fbb5f0921a0b672610f63833c8a8c41b812
yvesdylane <yvesdylane24@gmail.com>
2025-07-10 01:52:32 +0100

    test

--- controllers/event.controller.ts
+++ controllers/event.controller.ts
@@ -1,6 +1,7 @@
 import { Context } from "../deps.ts";
 import { getAll, getEventById, getParticipants } from "../services/event.service.ts";
 import { Event } from '../models/event.model';
+import { participant } from '../models/participant.model';
 
 export async function getEvents(ctx: Context) {
     try {
@@ -45,16 +46,22 @@ export async function getEventParticipants(ctx: Context) {
     }
 }
 
+interface EventFullInfo {
+    event: Event | null;
+    participants: Array<participant> | null;
+}
+
 export async function getEventInfoAndParticipant(ctx: Context) {
     const { id } = await ctx.request.body({ type: "json" }).value;
-    interface eventFullInfo={
-        event: null as Event | null,
-        participants: null as Array<{ user_id: number, user_name: string }> | null
-    }
     try {
-        eventFullInfo.event = await getEventById(id);
-        eventFullInfo.participants = await getParticipants(id);
-        
+        const event = await getEventById(id);
+        const participants = await getParticipants(id);
+
+        const eventFullInfo: EventFullInfo = {
+            event: event,
+            participants: participants
+        };
+
         ctx.response.status = 200;
         ctx.response.body = eventFullInfo;
     } catch (error) {
