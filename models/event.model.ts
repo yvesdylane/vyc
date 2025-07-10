1d3ef3d31cfdea800f9c51bbc8d8403b35a697ce
yvesdylane <yvesdylane24@gmail.com>
2025-07-10 01:50:02 +0100

    events

--- models/event.model.ts
+++ models/event.model.ts
@@ -7,7 +7,6 @@ export interface Scope {
 export interface Event {
     id: number; 
     institution_id: string; // UUID of the institution, this is a reference to the institution that created the event
-    name: string;
     create_by: string; // UUID of the user who created the event
     created_at: string; // ISO timestamp string
     updated_at: string; // ISO timestamp string
@@ -18,5 +17,6 @@ export interface Event {
     vote_cost?: number; // optional, the cost of voting for this event
     maximum_vote?: number; // optional, the maximum number of votes allowed for this event
     who_can_participate?: Scope; // optional, JSON object defining who can participate in the event
+    name: string;
 }
 
